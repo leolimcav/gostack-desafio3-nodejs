18f3a6f570b4887190002b5a86279b1058e22c94
Leonardo <leonardo123k@gmail.com>
2020-05-27 18:10:54 -0300

    adicionado multer e leitura de csv

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
+import multer from 'multer';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+
+import uploadConfig from '../config/upload';
 
 const transactionsRouter = Router();
+const upload = multer(uploadConfig);
 
 transactionsRouter.get('/', async (request, response) => {
   const transactionsRepository = getCustomRepository(TransactionsRepository);
@@ -42,8 +46,20 @@ transactionsRouter.delete('/:id', async (request, response) => {
   return response.status(204).send();
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('csv'),
+  async (request, response) => {
+    const { filename } = request.file;
+
+    const importTransaction = new ImportTransactionsService();
+
+    const transactions = await importTransaction.execute({
+      fileName: filename,
+    });
+
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,53 @@
+import csvParse from 'csv-parse';
+import path from 'path';
+import fs from 'fs';
+
+import { getRepository } from 'typeorm';
+
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+import uploadConfig from '../config/upload';
+
+interface Request {
+  fileName: string;
+}
+
+interface CSVTransaction {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute({ fileName }: Request): Promise<Transaction[]> {
+    const transactionsRepository = getRepository(Transaction);
+    const categoriesRepository = getRepository(Category);
+
+    const csvFilePath = path.resolve(uploadConfig.directory, fileName);
+
+    const readCSVStream = fs.createReadStream(csvFilePath);
+
+    const parseStream = csvParse({
+      from_line: 2,
+      ltrim: true,
+      rtrim: true,
+    });
+
+    const parseCSV = readCSVStream.pipe(parseStream);
+
+    const transactions: CSVTransaction[] = [];
+    const categories: string[] = [];
+
+    parseCSV.on('data', line => {
+      const [title, type, value, category] = line;
+
+      categories.push(category);
+      transactions.push({ title, type, value, category });
+    });
+
+    await new Promise(resolve => parseCSV.on('end', resolve));
   }
 }
 
